55524796cabddd15c18bc93580ebafdc93440080
Aziz Ali <aziz@ilovecoding.org>
2020-09-16 09:19:40 -0500

    For While Do-While Loops

--- code.js
+++ code.js
@@ -1,60 +1,29 @@
-// // IF STATEMENT
-// var age = 35;
+// FOR LOOP
+//    1;    2   ;       4
+// for(var i = 0; i < 100; i++){
+//   console.log(i);     // 3
+// }
 
-// if(age < 18){
-//   console.log('You can NOT buy insurance');
-// } else if(
-//   (age >= 18 && age < 21)
-//   ||
-//   (age >= 30 && age < 35)
-// ) {
-//   console.log('You need parental guidance');
-// } else {
-//   console.log('You can buy insurance');
+// console.log('line after the loop', i);
+
+// var arr = ['A', 'B', 'C', 1,,5,5,5,5];
+// for(item in arr){
+//   console.log(arr[item]);
+// }
+
+// WHILE LOOP
+// var i = 0;
+// while(i < 100){
+//   console.log(i);
+//   i++;
 // }
 
 
-// Truthy and Falsey Values
-// if(''){
-//   console.log('if');
-// } else {
-//   console.log('else');
-// }
-
-// var userList = [];
-
-// if(userList){
-//   console.log('User info')
-// } else {
-//   console.log('Is loading')
-// }
+// DO WHILE LOOP
+var i = 0;
+do {
+  console.log(i)
+  i++;
+} while(i<100)
 
 
-// SWITCH STATEMENTS
-// var age = 18;
-// switch (age) {
-//   case 18:
-//     console.log('You need parental guidance');
-//     break;
-//   case 18:
-//     console.log('You can buy insurance');
-//     break;
-//   default:
-//     console.log('We do not have a match for you');
-//     break;
-// }
-
-// Multiple Scenarios
-// switch (true) {
-//   case age > 18 && age <= 21: {
-//   }
-
-var userList = null
-
-var hasUsers = (userList)? 'Hi' : 'Bye';
-
-if(userList){
-  var hasUsers = hi;
-} else {
-  var hasUsers = 'Bye';
-}
